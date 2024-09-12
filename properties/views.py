efe0218b5236bab953c6b363ff924608d52f83c9
abdullah956 <abdullaharshed956@gmail.com>
2024-09-12 09:13:30 +0500

    listing

--- properties/views.py
+++ properties/views.py
@@ -3,4 +3,4 @@ from .models import Property
 
 def approved_properties_view(request):
     properties = Property.objects.filter(approval_status=True)
-    return render(request, 'listings.html', {'properties': properties})
+    return render(request, 'properties/listings.html', {'properties': properties})
