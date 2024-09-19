1f5a5ce134bf8a4294e94d685d72cdb6d187316e
abdullah956 <abdullaharshed956@gmail.com>
2024-09-19 14:35:04 +0500

    status show for AG of H

--- agent/urls.py
+++ agent/urls.py
@@ -1,15 +1,17 @@
 from django.urls import path
-from .views import all_homeowners,homeowner_send_connection_request,dashboard_view, all_assistants , assistant_send_connection_request
+from .views import all_homeowners,homeowner_requests_status_by_agent,homeowner_send_connection_request,dashboard_view, all_assistants , assistant_send_connection_request
 
 urlpatterns = [
     # dashboard for agent and assistant
     path('agent-dashboard/', dashboard_view, name='dashboard'),
     # to see all homeowners
-    path('all_homeowners/', all_homeowners, name='all_homeowners'),
+    path('all-homeowners/', all_homeowners, name='all_homeowners'),
     # to send invite to homeowners
-    path('homeowner_send_connection_request/', homeowner_send_connection_request, name='homeowner_send_connection_request'),
+    path('homeowner-send-connection-request/', homeowner_send_connection_request, name='homeowner_send_connection_request'),
     # to see all assistants
-    path('all_assistants/', all_assistants, name='all_assistants'),
+    path('all-assistants/', all_assistants, name='all_assistants'),
     # to send invite to assistants
-    path('assistant_send_connection_request/', assistant_send_connection_request, name='assistant_send_connection_request'),
+    path('assistant-send-connection-request/', assistant_send_connection_request, name='assistant_send_connection_request'),
+    # to see the status of request of homeowners
+    path('homeowner-requests-status-by-agent/', homeowner_requests_status_by_agent, name='homeowner_requests_status_by_agent'),
 ]
