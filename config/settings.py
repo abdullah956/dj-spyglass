c69681bb3f157f0f17cb296363df9f3a889c93e4
abdullah956 <abdullaharshed956@gmail.com>
2024-10-19 22:12:54 +0500

    live

--- config/settings.py
+++ config/settings.py
@@ -143,8 +143,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'abdullaharshed956@gmail.com'
-EMAIL_HOST_PASSWORD = 'kyjdwlhepwzwptsm'
+EMAIL_HOST_USER = 'verification@transparagent.com'
+EMAIL_HOST_PASSWORD = 'lgywdlctvrpuqmeb'
 
 # settings.py
 OTP_SECRET_KEY = 'base32secret3232'
