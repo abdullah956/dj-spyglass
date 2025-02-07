ccb85b1106bc0c092fd4c1a386c42076dfeac5b4
abdullah956 <abdullaharshed956@gmail.com>
2025-02-07 15:54:44 +0500

    CR removed admin

--- properties/admin.py
+++ properties/admin.py
@@ -2,6 +2,5 @@ from django.contrib import admin
 from .models import Property , ConnectionRequest,AgentInvitation, Favorites
 
 admin.site.register(Property)
-admin.site.register(ConnectionRequest)
 admin.site.register(AgentInvitation)
 admin.site.register(Favorites)
